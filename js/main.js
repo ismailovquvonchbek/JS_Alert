3e28120e28d7ef86a64ceefc3bb15a56f0a4bfcb
ismailovquvonchbek <ismailovquvonchbek08@gmail.com>
2023-11-13 20:10:18 +0500

    Alert

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var UserNomer = Number(prompt("Son yozing..." , 6))
+var UserNomers = Number(prompt("Son yozing..." , 4))
+ 
+var Javob = UserNomer / UserNomers
+
+var Bu =  Math.floor(Javob)
+
+alert(Bu)
